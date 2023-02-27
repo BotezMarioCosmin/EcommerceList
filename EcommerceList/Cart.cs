a911c9d056fb25d0a935f0d39939531718901e89
Botez Mario Cosmin <botezmariocosmin@gmail.com>
2023-02-27 09:01:22 +0100

    carrello

---
+++ EcommerceList/Cart.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ecommerce2
+{
+    public class Cart
+    {
+        //attributi
+        private const int MAXCARR = 999;
+        private string _id;
+        private int currentLenght;
+        private Product[] _prod = new Product[MAXCARR];
+
+        List<Product> Prodotti = new List<Product>();
+
+        //properties
+        public string Id
+        {
+            get
+            {
+                return _id;
+            }
+            private set
+            {
+                if (value != null)
+                    _id = value;
+                else
+                    throw new Exception("Invalid Id");
+            }
+        }
+
+        public Product[] Products
+        {
+            get
+            {
+                Product[] p = new Product[currentLenght];
+                for (int i = 0; i < currentLenght; i++)
+                {
+                    p[i] = _prod[i];
+                }
+                return p;
+            }
+            //set{
+            // 
+            //}
+        }
+
+        public Cart(string id)
+        {
+            this.Id = id;
+            Clear();
+        }
+
+        public void Clear()
+        {
+            Prodotti.Clear();
+        }
+        public void Add(Product p)
+        {
+            if (p != null)
+            {
+                Prodotti.Add(p);
+            }
+            else
+                throw new Exception("Invalid product");
+        }
+
+        private int Length()
+        {
+            return Prodotti.Count();
+        }
+
+        public int IndexOf(Product q)
+        {
+            return Prodotti.IndexOf(q);
+        }
+
+        public void Modify(Product p)
+        {
+            int i = IndexOf(p);
+            if (i >= 0)
+            {
+                Prodotti.Add(p);
+            }
+            else
+                throw new Exception("Product not found");
+        }
+
+        public Product Remove(Product p)
+        {
+            if (IndexOf(p) != -1)
+            {
+                Prodotti.Remove(p);
+
+                return p;
+            }
+            else
+                throw new Exception("Product not found");
+        }
+    }
+}
+
---
+++ EcommerceList/Product.cs
@@ -0,0 +1,158 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ecommerce2
+{
+    public abstract class Product
+    {
+        //attributi
+        private string _id, _name, _manufacturer, _description;
+        private float _price;
+
+        //properties
+        public float Price
+        {
+            get
+            {
+                return _price;
+            }
+            set
+            {
+                if (value > 0)
+                    _price = value;
+                else
+                    throw new Exception("Price must be >0");
+            }
+        }
+
+        public string Id
+        {
+            get
+            {
+                return _id;
+            }
+            private set
+            {
+                if (value != null)
+                    _id = value;
+                else
+                    throw new Exception("Invalid ID");
+            }
+        }
+
+        public string Name
+        {
+            get
+            {
+                return _name;
+            }
+            private set
+            {
+                if (value != null)
+                    _name = value;
+                else
+                    throw new Exception("Invalid name");
+            }
+        }
+
+        public string Manufacturer
+        {
+            get
+            {
+                return _manufacturer;
+            }
+            private set
+            {
+                if (value != null)
+                    _manufacturer = value;
+                else
+                    throw new Exception("Invalid manufacturer");
+            }
+        }
+
+        public string Description
+        {
+            get
+            {
+                return _description;
+            }
+            private set
+            {
+                if (value != null)
+                    _description = value;
+                else
+                    throw new Exception("Invalid description");
+            }
+        }
+
+        //costruttori
+        public Product(string id, string name, string prod, string descr, float price)
+        {
+            if (String.IsNullOrEmpty(id) || String.IsNullOrEmpty(name) || String.IsNullOrEmpty(prod) || price <= 0)
+            {
+                throw new Exception("Invalid product parameters ");
+            }
+            Id = id;
+            Name = name;
+            Manufacturer = prod;
+            Description = descr;
+            Price = price;
+        }
+
+        public Product(string id, string name, string prod, string descr) : this(id, name, prod, descr, 10)
+        {
+        }
+
+        public Product(string id, string name, string prod) : this(id, name, prod, "N/A", 10)
+        {
+        }
+
+        public Product() : this("IDVUOTO", "N/A", "N/A", "N/A", 10)
+        {
+        }
+
+        public Product(string id, string name, string prod, float price) : this(id, name, prod, "N/A", price)
+        {
+
+        }
+
+        public Product(string id) : this(id, "N/A", "N/A", "N/A", 1)
+        {
+
+        }
+
+        //costruttore copia
+        protected Product(Product other) : this(other.Id, other.Name, other.Manufacturer, other.Description, other.Price)
+        {
+        }
+
+        //clone 
+        /*
+        public Product Clone()
+        {
+            return new Product(this);
+        }
+        */
+        //Equals
+        public bool Equals(Product p)
+        {
+            if (p == null) return false;
+
+            if (this == p) return true;
+
+            return (this.Id == p.Id);
+        }
+
+        //ToString
+        public override string ToString()
+        {
+            return "Product:" + Id + ";" + Name + ";" + Manufacturer + ";" + Description + ";" + Price;
+        }
+
+        public virtual float getDiscount() { return Price; }
+    }
+}
+
